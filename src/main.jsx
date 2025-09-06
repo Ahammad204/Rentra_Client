705752dcf32ade3ea6bf0e30ca63ef7e34946a37
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-09-06 20:26:22 +0600

    Create responsive navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './Router/Router'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 
 createRoot(document.getElementById('root')).render(
